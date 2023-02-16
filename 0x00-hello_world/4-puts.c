41a097b67e44e5de249e81e2416b5e6be2600a1e
Shirley <blackwidow.dk@gmail.com>
2023-02-16 11:12:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 /**
 * main - main block
-* Return: o */ int main(void)
+* Return: o
+*/
+int main(void)
 { puts("\"Programming is like building a multilingual puzzle"); return(0); }
