60f8c9f32c9b36a467d87443462da4089bccfd1c
Shirley <blackwidow.dk@gmail.com>
2023-02-21 07:13:14 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * _islower -> checks if the character is lowercase
-* Return: returns 0 or 1 depending on condition
+* Return: returns 1 or 0 depending on condition
 */
 int _islower(int c)
 {
-return(c >= 'a' && c <= 'z')
+return (c >= 'a' && c <= 'z');
 }
