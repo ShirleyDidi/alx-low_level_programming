6bd3aeb5050a824ede172c2bb06b38fda7ac3b5d
Shirley <blackwidow.dk@gmail.com>
2023-02-21 16:04:17 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,25 @@
 #include "main.h"
 /**
-* jack_bauer -> prints 24 hours
+* jack_bauer - prints every minutes of the day of Jack Bauer
+* starting from 00:00 to 23:59
 */
 void jack_bauer(void)
 {
 int i, j;
-for (i = 0; i < 24; i++)
+i = 0;
+while (i < 24)
 {
-for (j = 0; j < 60; j++)
+j = 0;
+while (j < 60)
 {
-if (i < 10)
-{
-_putchar('0');
-_putchar(i + '0');
-}
-else if (i >= 10)
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-}
-if (j < 10)
-{
-_putchar(':');
-_putchar('0');
-_putchar(j + '0');
-}
-else if (j >= 10)
-{
 _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
-}
 _putchar('\n');
+j++;
+}
+i++;
 }
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,10 +2,10 @@
 /**
 * times_table - prints the 9 times table, starting with 0
 */
-void time_table(void)
+void times_table(void)
 {
-int i, j k;
-for (i =0; i < 10; i++)
+int i, j, k;
+for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
