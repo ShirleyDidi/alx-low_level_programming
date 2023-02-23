363bbd44a08709be8d3fec1b039d450e9fe5fd80
Shirley <blackwidow.dk@gmail.com>
2023-02-23 01:56:02 -0800

    more_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* prints the numbers, from 0 to 9, followed by a new line.
+* @x: The neumber to be checked
+* Return: The numbers since 0 up to 9
+*/
+void print_numbers(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
+}
