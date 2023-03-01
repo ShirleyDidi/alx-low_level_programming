a689a7ba15d6ed6f4facd53447e62d9b354bce2b
Shirley <blackwidow.dk@gmail.com>
2023-03-01 10:01:17 -0800

    leet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i])
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'))
+while (!(str[i] >= 'a' && str[i] <= 'Z'))
 i++;
 if (str[i - 1] == ' ' ||
 str[i - 1] == '\t' ||
