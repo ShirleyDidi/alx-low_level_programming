8f674b38c8164c6f05b8904d707321a657ac4b0a
Shirley <blackwidow.dk@gmail.com>
2023-03-20 05:24:18 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include "dog.h"
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
 * _strlen - returns the length of a string
 * @s: string to evaluate
@@ -36,6 +39,7 @@ dest[i] = src[i];
 dest[i] = '\0';
 return (dest);
 }
+
 /**
 * new_dog - new dog
 * @name: name of a dog
@@ -56,10 +60,11 @@ if (dog == NULL)
 return (NULL);
 dog->name = malloc(sizeof(char) * name_l);
 if (dog->name == NULL)
+{
 free(dog);
 return (NULL);
 }
-dog->owner = malloc(sizeof(char) * (own_l + 1));
+dog->owner = malloc(sizeof(char) * own_l);
 if (dog->owner == NULL)
 {
 free(dog->name);
@@ -69,5 +74,6 @@ return (NULL);
 dog->name = _strcpy(dog->name, name);
 dog->owner = _strcpy(dog->owner, owner);
 dog->age = age;
+}
 return (dog);
 }
