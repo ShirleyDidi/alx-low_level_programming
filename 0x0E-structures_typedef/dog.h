5ee0b024bb9162f2607b25763febd9d4fc6674b0
Shirley <blackwidow.dk@gmail.com>
2023-03-20 00:48:06 -0700

    dog.h_init_dog_print_dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+/**
+* struct dog - structure dog
+* @name: attributes pointer to char name's dog
+* @age: attributes float age's dog
+* @owner: attributes pointer to char owners dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+* dog_t - typedef fr dog structure
+*/
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
