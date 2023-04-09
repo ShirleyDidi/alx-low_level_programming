1996cfa11aa29925731a6980245bdd6512ebc4a3
ShellDidi <blackwido.dk@gmail.com>
2023-04-09 04:49:32 -0700

    bit-manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,8 +15,10 @@ return (0);
 for (a = 0; b[a] != '\0'; a++)
 {
 if (b[a] < '0' || b[a] > '1')
+{
 return (0);
 }
 art = (art << 1) | (b[a] - '0');
+}
 return (art);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,9 @@
 void print_binary(unsigned long int n)
 {
 unsigned long int hop;
+int b;
 if (n == 0)
-putchar('0');
+_putchar('0');
 return;
 while ((hop & n) == 0)
 {
@@ -18,11 +19,9 @@ hop >>= 1;
 while (hop != 0)
 {
 if ((hop & n) == 0)
-putchar('0');
-}
-else if
-{
-putchar('1');
+_putchar('0');
+else
+_putchar('1');
 }
 hop >>= 1;
 }
