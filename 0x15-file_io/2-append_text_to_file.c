e8aef1a233057e2f512bf94ccc9135d4d4ef1e0b
ShellDidi <blackwido.dk@gmail.com>
2023-04-10 06:51:53 -0700

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,6 +5,7 @@
 /**
 * append_text_to_file - appends text at the end of a file.
 * @filename: name of a file
+* @text_content: content of text
 * Return: 1 on success and -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
