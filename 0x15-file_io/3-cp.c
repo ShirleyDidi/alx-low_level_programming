6dcce7b6cba5ea5a7cc4cc805e8b872daf4da001
ShellDidi <blackwido.dk@gmail.com>
2023-04-10 08:15:25 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#define BUF_SIZE 1024
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
 /**
 * cp - copies the content of a file to another file.
-* 
+* @file_from: does not exist, or if you can not read it, exit with code 98
+* @file_to: already exists, truncate it 
 */
-#define BUF_SIZE 1024
-
 int main(int argc, char *argv[])
 {
 int mod, mon;
 ssize_t numb_read;
 ssize_t numb_written;
+char *buf;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
@@ -28,11 +34,10 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(mod);
 exit(99);
 }
-char buf[BUF_SIZE];
-numb_read;
+buf = calloc(1024, sizeof(char));
 while ((numb_read = read(mod, buf, BUF_SIZE)) > 0)
 {
-numb_written = write(fd_to, buf, numb_read);
+numb_written = write(mod, buf, numb_read);
 if (numb_written != numb_read)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -41,7 +46,7 @@ close(mon);
 exit(99);
 }
 }
-if (num_read == -1)
+if (numb_read == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 close(mod);
